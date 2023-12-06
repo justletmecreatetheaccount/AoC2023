9aff7c4932a70b15be1104d67cc7f37bac5aa0c1
[Vlad Doniga] <[vladdoniga11@gmail.com]>
2023-12-06 11:06:57 +0100

    exeptionaly ugly solution for day 4

--- AoC-3/src/main.rs
+++ AoC-3/src/main.rs
@@ -164,5 +164,153 @@ fn main() {
     }
     println!("Sum : {}", sum);
 
-    
+    for i in 0..chars_array.len() {
+        for j in 0..chars_array[i].len() {
+            match chars_array[i][j].to_digit(10) {
+                Some(x) => {
+                    if temp == 0 {
+                        
+                        temp += x as i32;
+
+                        //Chexk up and diagonal
+                        let t = i as i64 - 1;
+                        println!("{:?} with i worth {:?}", t as usize, i);
+                        match chars_array.get(t as usize) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i - 1][j], chars_array[i][j], temp);
+                                if chars_array[i - 1][j] == '*' {
+                                    next_to = true;
+                                }
+                                let t = j as i64 - 1;
+                                match chars_array[i - 1].get(t as usize) {
+                                    Some(_char) => {
+                                        println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i - 1][j - 1], chars_array[i][j], temp);
+                                        if chars_array[i - 1][j - 1] == '*' {
+                                            next_to = true;
+                                        }
+                                    },
+                                    None => {
+                                        println!("number {:?} is on the left edge", temp);
+                                    },
+                                }
+                            },
+                            None => {
+                                println!("number {:?} is on the top edge", temp);
+                            },
+                        }
+                        //Check down and diagonal
+                        match chars_array.get(i + 1) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i + 1][j], chars_array[i][j], temp);
+                                if chars_array[i + 1][j] == '*' {
+                                    next_to = true;
+                                }
+                                let t = j as i64 - 1;
+                                match chars_array[i + 1].get(t as usize) {
+                                    Some(_char) => {
+                                        println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i + 1][j - 1], chars_array[i][j], temp);
+                                        if chars_array[i + 1][j - 1] == '*' {
+                                            next_to = true;
+                                        }
+                                    },
+                                    None => {
+                                    },
+                                }
+                            },
+                            None => {
+                            },
+                        }
+
+                        //Check left (actually right)
+                        let t = j as i64 - 1;
+                        match chars_array[i].get(t as usize) {
+                            Some(_char) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i][j - 1], chars_array[i][j], temp);
+                                if chars_array[i][j - 1] == '*' {
+                                    next_to = true;
+                                }
+                            },
+                            None => {
+                            },
+                        }
+
+
+                    } else {
+
+
+                        temp *= 10;
+                        temp += x as i32;
+
+                        //Chexk up
+                        let t = i as i64 - 1;
+                        match chars_array.get(t as usize) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i - 1][j], chars_array[i][j], temp);
+                                if chars_array[i - 1][j] == '*' {
+                                    next_to = true;
+                                }
+                            },
+                            None => {
+                            },
+                        }
+                        //Check down
+                        match chars_array.get(i + 1) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i + 1][j], chars_array[i][j], temp);
+                                if chars_array[i + 1][j] == '*' {
+                                    next_to = true;
+                                }
+                            },
+                            None => {
+                            },
+                        }
+
+
+                    }
+                },
+                None => {
+                    if temp > 0 {
+                        //Chexk up
+                        let t = i as i64 - 1;
+                        match chars_array.get(t as usize) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i - 1][j], chars_array[i][j], temp);
+                                if chars_array[i - 1][j] == '*' {
+                                    next_to = true;
+                                }
+                            },
+                            None => {
+                            },
+                        }
+                        //Check down
+                        match chars_array.get(i + 1) {
+                            Some(_line) => {
+                                println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i + 1][j], chars_array[i][j], temp);
+                                if chars_array[i + 1][j] == '*' {
+                                    next_to = true;
+                                }
+                            },
+                            None => {
+                            },
+                        }
+                        //Check middle
+                        println!("{:?} is checked while on number {:?} and total {:?}", chars_array[i][j], chars_array[i][j], temp);
+                        if chars_array[i][j] == '*' {
+                            next_to = true;
+                        }
+
+                        if next_to {
+                            println!("{} is next to a symbol", temp);
+                            sum += temp;
+                            temp = 0;
+                            next_to = false;
+                        } else {
+                            println!("{} is not next to a symbol", temp);
+                            temp = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
